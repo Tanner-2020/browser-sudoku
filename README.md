be77e9f17186f8c498874be80d44e5fe65015121
Tanner2020 <tanjhall2020@gmail.com>
2024-02-05 13:59:27 -0600

    Implemented puzzle generation
    Added function to generate diagonal squares randomly.
    Implemented modified solver function to create the rest of the grid.
    Updated README.md to reflect changes.

--- README.md
+++ README.md
@@ -6,5 +6,10 @@ ReDoKu is a web-based Sudoku game developed using React.js.
 - Enables solution checking of user answer compared to known solution.
 - Displays Sudoku rules to the user as well as basic strategy.
 - Displays various warnings for events that result in resetting the Sudoku board.
+- Generates a unique sudoku puzzle each time a new game is started.
 
+## Resources
+- sudokuSolver and isValid functions in generator are modified implementations code by of "mojtaba ramezani" on Stack Overflow.
+- Thanks to "Damzaky" for helping resolve issues with `.slice()` on 2D-arrays.
+- Thanks to "James" on Stack Overflow for help with implementing the user solution saving.
 ### Developed by Tanner Hall, 2024
--- src/Generator.js
+++ src/Generator.js
@@ -1,14 +1,19 @@
+var grid;
 export function generateSudokuTable(difficulty){
 
-    let grid = [[7,2,6,3,5,9,4,1,8],
-                [4,5,8,1,6,7,2,3,9],
-                [9,1,3,8,2,4,7,6,5],
-                [1,6,2,9,7,5,3,8,4],
-                [3,9,4,2,8,6,1,5,7],
-                [8,7,5,4,1,3,9,2,6],
-                [5,3,7,6,4,1,8,9,2],
-                [6,8,9,7,3,2,5,4,1],
-                [2,4,1,5,9,8,6,7,3]];
+    grid = [[0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0]];
+
+    generateDiagonal();
+    sudokuSolver(grid);
+    console.log(grid);
 
     // Sets number of values to remove for the puzzle table.
     let removedValues = 0;
@@ -44,4 +49,59 @@ export function generateSudokuTable(difficulty){
         }
     }
     return [grid, puzzle];
+}
+
+function generateDiagonal(){
+    for(let i = 0; i < 3; i ++){
+        let numList = generateList();
+        for(let j = 0; j < 9; j++){
+            grid[(i*3)+Math.floor(j/3)][(i*3)+j%3] = numList[j];
+        }
+    }
+}
+
+function generateList(){
+    let list = [1,2,3,4,5,6,7,8,9];
+    for(let i = 0; i < 9; i++){
+        let temp = list[i]
+        let randomIndex = Math.floor(Math.random()*9);
+        list[i] = list[randomIndex];
+        list[randomIndex] = temp;
+    }
+    return list;
+}
+
+
+// Modified off of code by user "mojtaba ramezani" on Stack Overflow
+function isValid(board, row, col, k) {
+    for (let i = 0; i < 9; i++) {
+        const m = 3 * Math.floor(row / 3) + Math.floor(i / 3);
+        const n = 3 * Math.floor(col / 3) + i % 3;
+        if (board[row][i] === k || board[i][col] === k || board[m][n] === k) {
+          return false;
+        }
+    }
+    return true;
+}
+
+// Modified off of code by user "mojtaba ramezani" on Stack Overflow
+function sudokuSolver(data) {
+  for (let i = 0; i < 9; i++) {
+    for (let j = 0; j < 9; j++) {
+      if (data[i][j] === 0) {
+        for (let k = 1; k <= 9; k++) {
+          if (isValid(data, i, j, k)) {
+            data[i][j] = parseInt(`${k}`);
+          if (sudokuSolver(data)) {
+           return true;
+          } else {
+           data[i][j] = 0;
+          }
+         }
+       }
+       return false;
+     }
+   }
+ }
+ return true;
 }
